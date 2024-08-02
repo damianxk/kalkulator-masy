5b76f68e12a60648ef55d5f72d94a4d1ca0f6ffe
Jakub Karp <kontakt@kubakarp.pl>
2024-08-02 09:25:37 +0200

    dopasowanie postów pod mobilne

--- src/components/MobileTopBar/MobileTopBar.jsx
+++ src/components/MobileTopBar/MobileTopBar.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { useEffect, useState } from 'react';
-import mimg1 from '../../assets/mslide1.png';
-import mimg2 from '../../assets/mslide2.png';
+import img1 from '../../assets/carport-1100x300.png';
+import img2 from '../../assets/mslide1.png';
+import img3 from '../../assets/mslide2.png';
+import css from './MobileTopBar.module.css'
+
 
 
 const MobileTopBar = () => {
     const [currentIndex, setCurrentIndex] = useState(0);
     const images = [
-        mimg1,
-        mimg2
+      {img: img1, link: 'https://ulamex.com.pl/carport'},
+      {img: img2, link: ''},
+      {img: img3, link: ''},
     ];
     useEffect(() => {
         const interval = setInterval(() => {
@@ -18,7 +22,9 @@ const MobileTopBar = () => {
     }, [images.length]);
   return (
     <div className="slider">
-      <img src={images[currentIndex]} alt={`Slide ${currentIndex}`} width={'100%'} height={'100%'}/>
+      <a href={images[currentIndex].link}>
+        <img src={images[currentIndex].img} alt={`Slide ${currentIndex}`} width={'100%'} height={'100%'}/>
+      </a>
     </div>
   )
 }
--- src/components/RightBar/RightBar.jsx
+++ src/components/RightBar/RightBar.jsx
@@ -1,15 +1,15 @@
 import React, { useEffect } from 'react';
 import { useState } from 'react';
-import img1 from '../../assets/slide1.jpg';
+import img1 from '../../assets/carport-300x1100.png';
 import img2 from '../../assets/slide2.jpg';
 import img3 from '../../assets/slide3.jpg';
 
 const RightBar = () => {
     const [currentIndex, setCurrentIndex] = useState(0);
     const images = [
-        img1,
-        img2,
-        img3
+        {img: img1, link: 'https://ulamex.com.pl/carport'},
+        {img: img2, link: ''},
+        {img: img3, link: ''},
     ];
     useEffect(() => {
         const interval = setInterval(() => {
@@ -19,7 +19,9 @@ const RightBar = () => {
     }, [images.length]);
   return (
     <div className="slider">
-      <img src={images[currentIndex]} alt={`Slide ${currentIndex}`} width={'100%'} height={'100%'}/>
+      <a href={images[currentIndex].link}>
+        <img src={images[currentIndex].img} alt={`Slide ${currentIndex}`} width={'100%'} height={'100%'}/>
+      </a>
     </div>
   )
 }
