9e222f8a13cf14c07e11c312c1cdd14810625146
Jakub Karp <73857419+jakub-karp@users.noreply.github.com>
2024-09-26 08:44:56 +0200

    podmiana postów na nowe

--- src/assets/posts.tsx
+++ src/assets/posts.tsx
@@ -1,26 +1,27 @@
 export const posts = [
+  {
+    title: "ILE WAŻĄ PRĘTY ZBROJENIOWE?",
+    desc: "Pręty zbrojeniowe to jeden z kluczowych elementów konstrukcyjnych stosowanych w budownictwie.",
+    imgpath:
+      "https://www.kalkulatorstali.com/blog/wp-content/uploads/2024/08/ile-waza-prety-zbrojeniowe-768x513.jpg",
+    date: "30 sierpnia, 2024",
+    link: "https://www.kalkulatorstali.com/blog/2024/08/30/ile-waza-prety-zbrojeniowe/",
+  },
+  {
+    title: "NAJCZĘŚCIEJ ZADAWANE PYTANIA",
+    desc: "W tym artykule zebraliśmy dla Ciebie najczęściej zadawane pytania. Starannie opracowaliśmy poniższy zestaw pytań i odpowiedzi.",
+    imgpath:
+      "https://www.kalkulatorstali.com/blog/wp-content/uploads/2024/08/najczesciej-zadawane-pytania-768x513.jpg",
+    date: "30 sierpnia, 2024",
+    link: "https://www.kalkulatorstali.com/blog/2024/08/30/najczesciej-zadawane-pytania/",
+  },
     {
-      title: "PRZELICZNIK STALI I ALUMINIUM",
-      desc: "W przemyśle metalurgicznym, czy budowlanym używane są przeróżne materiały, jednak stal i aluminium są jednymi z najważniejszych.",
+      title: "RODZAJE I WŁAŚCIWOŚCI …",
+      desc: "Dwuteowniki to jedne z najważniejszych elementów konstrukcyjnych stosowanych w budownictwie i przemyśle.",
       imgpath:
-        "https://www.kalkulatorstali.com/blog/wp-content/uploads/2024/08/przelicznik-stali-i-aluminium-768x513.png",
-      date: "26 sierpnia, 2024",
-      link: "https://www.kalkulatorstali.com/blog/2024/08/26/przelicznik-stali-i-aluminium-narzedzie-niezbedne-dla-przemyslu/",
+        "https://www.kalkulatorstali.com/blog/wp-content/uploads/2024/09/worker-is-controlling-process-rail-cutting-busy-metal-factory-768x512.jpg",
+      date: "26 września, 2024",
+      link: "https://www.kalkulatorstali.com/blog/2024/09/26/rodzaje-i-wlasciwosci-dwuteownikow/",
     },
-    {
-      title: "ILE WAŻĄ PRĘTY ZBROJENIOWE?",
-      desc: "Pręty zbrojeniowe to jeden z kluczowych elementów konstrukcyjnych stosowanych w budownictwie.",
-      imgpath:
-        "https://www.kalkulatorstali.com/blog/wp-content/uploads/2024/08/ile-waza-prety-zbrojeniowe-768x513.jpg",
-      date: "30 sierpnia, 2024",
-      link: "https://www.kalkulatorstali.com/blog/2024/08/30/ile-waza-prety-zbrojeniowe/",
-    },
-    {
-      title: "NAJCZĘŚCIEJ ZADAWANE PYTANIA",
-      desc: "W tym artykule zebraliśmy dla Ciebie najczęściej zadawane pytania. Starannie opracowaliśmy poniższy zestaw pytań i odpowiedzi.",
-      imgpath:
-        "https://www.kalkulatorstali.com/blog/wp-content/uploads/2024/08/najczesciej-zadawane-pytania-768x513.jpg",
-      date: "30 sierpnia, 2024",
-      link: "https://www.kalkulatorstali.com/blog/2024/08/30/najczesciej-zadawane-pytania/",
-    },
+    
   ];
