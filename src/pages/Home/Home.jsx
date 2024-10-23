908a79be258626f6e0e927bc2fcd5ff08278ee45
Damian Kowalczyk <damiank209@gmail.com>
2024-10-23 10:49:19 +0200

    nowe artykuły

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -16,30 +16,31 @@ const Home = () => {
   const [density, setDensity] = useState("2.7");
   const posts = [
     {
-      title: "WPROWADZENIE DO KALKULATORA STALI",
-      desc: "Stal nierdzewna, znana również jako INOX, to prawdziwy fenomen w świecie materiałów. Co sprawia, że jest tak wyjątkowa?",
+      title: "ILE WAŻĄ PRĘTY ZBROJENIOWE?",
+      desc: "Pręty zbrojeniowe to jeden z kluczowych elementów konstrukcyjnych stosowanych w budownictwie.",
       imgpath:
-        "https://www.kalkulatorstali.com/blog/wp-content/uploads/2024/08/stalnierdzewna-1024x683.jpg",
-      date: "1 sierpnia, 2024",
-      link: "https://www.kalkulatorstali.com/blog/2024/08/01/stal-nierdzewna-niezwykly-material-ktory-warto-poznac/",
+        "https://www.kalkulatorstali.com/blog/wp-content/uploads/2024/08/ile-waza-prety-zbrojeniowe-768x513.jpg",
+      date: "30 sierpnia, 2024",
+      link: "https://www.kalkulatorstali.com/blog/2024/08/30/ile-waza-prety-zbrojeniowe/",
     },
     {
-      title: "KOMPLEKSOWY PRZEWODNIK",
-      desc: "Obliczanie wagi stali to kluczowy element w wielu dziedzinach, takich jak budownictwo, inżynieria, czy przemysł metalurgiczny.",
+      title: "NAJCZĘŚCIEJ ZADAWANE PYTANIA",
+      desc: "W tym artykule zebraliśmy dla Ciebie najczęściej zadawane pytania. Starannie opracowaliśmy poniższy zestaw pytań i odpowiedzi.",
       imgpath:
-        "https://www.kalkulatorstali.com/blog/wp-content/uploads/2024/08/stal-768x512.jpg",
-      date: "1 sierpnia, 2024",
-      link: "https://www.kalkulatorstali.com/blog/2024/08/01/kompleksowy-przewodnik-po-obliczaniu-wagi-stali-od-podstaw-do-zaawansowanych-technik/",
+        "https://www.kalkulatorstali.com/blog/wp-content/uploads/2024/08/najczesciej-zadawane-pytania-768x513.jpg",
+      date: "30 sierpnia, 2024",
+      link: "https://www.kalkulatorstali.com/blog/2024/08/30/najczesciej-zadawane-pytania/",
     },
-    {
-      title: "STAL NIERDZEWNA NIEZWYKŁY MATERIAŁ",
-      desc: "Stal nierdzewna, znana również jako INOX, to prawdziwy fenomen w świecie materiałów. Co sprawia, że jest tak wyjątkowa?",
-      imgpath:
-        "https://www.kalkulatorstali.com/blog/wp-content/uploads/2024/08/stalnierdzewna-768x512.jpg",
-      date: "1 sierpnia, 2024",
-      link: "https://www.kalkulatorstali.com/blog/2024/08/01/stal-nierdzewna-niezwykly-material-ktory-warto-poznac/",
-    },
-  ];
+      {
+        title: "RODZAJE I WŁAŚCIWOŚCI …",
+        desc: "Dwuteowniki to jedne z najważniejszych elementów konstrukcyjnych stosowanych w budownictwie i przemyśle.",
+        imgpath:
+          "https://www.kalkulatorstali.com/blog/wp-content/uploads/2024/09/worker-is-controlling-process-rail-cutting-busy-metal-factory-768x512.jpg",
+        date: "26 września, 2024",
+        link: "https://www.kalkulatorstali.com/blog/2024/09/26/rodzaje-i-wlasciwosci-dwuteownikow/",
+      },
+      
+    ];
 
   return (
     <>
